6e8781f29fb94abed4e43082c8d882646c8abf33
marygeorge257 <marygeorge25785@gmail.com>
2026-06-29 17:33:15 +0300

    main.dart

--- main.dart
+++ main.dart
@@ -4,6 +4,7 @@ import 'piano_cubit/result_dart_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:piano_app/piano_hive/progress_model.dart';
+import 'widgets/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,15 +13,16 @@ void main() async {
   await Hive.openBox<ProgressModel>('progressBox');
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => ResultDartCubit(),
-      child: MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      ),
     );
   }
 }
